cac75c264321f28be1b4708af00cdd65cced5606
Mark Richman <mark.richman@raise.com>
2015-04-26 20:18:12 -0500

    add routes, add views for new, index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,22 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(permitted_params)
+
+    redirect_to photos_url
+  end
+
+  def index
+    @photos = Photo.all
+  end
+
+  def permitted_params
+    params.require(:photo).permit(:owner, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Routes to READ photos
+  get("/photos/new", {:controller => "photos", :action => "new"})
   get("/photos/:id", { :controller => "photos", :action => "show" })
   get("/users/:id", { :controller => "users", :action => "people" })
+  post("/photos/", {:controller => "photos", :action => "create"})
+  get("/photos/", {:controller => "photos", :action => "index"})
 end
